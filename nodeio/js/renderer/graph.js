49242b0ee1c43ff9afe7ae88c70f739cd3d46d73
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2025-01-11 19:39:49 +0100

    Changed drop method

--- nodeio/js/renderer/graph.js
+++ nodeio/js/renderer/graph.js
@@ -10,4 +10,4 @@ const graph = /** @type {HTMLDivElement} */ (document.getElementById("graph"));
 export const DRAG_DROP_DATA_FORMAT = "text/plain";
 
 /** @param {DragEvent} e */
-graph.ondragend = e => renderNode(graph, e.dataTransfer?.getData(DRAG_DROP_DATA_FORMAT) ?? "");
+graph.ondrop = e => renderNode(graph, e.dataTransfer?.getData(DRAG_DROP_DATA_FORMAT) ?? "");
