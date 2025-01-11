6ff1edc6ed519827262c9eaa88f4086323cfa6d5
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2025-01-11 19:43:04 +0100

    Fix

--- nodeio/js/renderer/graph.js
+++ nodeio/js/renderer/graph.js
@@ -9,5 +9,6 @@ const graph = /** @type {HTMLDivElement} */ (document.getElementById("graph"));
 
 export const DRAG_DROP_DATA_FORMAT = "text/plain";
 
+graph.ondragover = e => e.preventDefault();
 /** @param {DragEvent} e */
 graph.ondrop = e => renderNode(graph, e.dataTransfer?.getData(DRAG_DROP_DATA_FORMAT) ?? "");
